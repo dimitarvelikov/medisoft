c11a86f26cc4b64df775899f6cd6cd63f32e2cdf
Dimitar Vasilev <dimityrv87@gmail.com>
2023-08-26 00:26:32 +0300

    Address and Patient creation

---
+++ src/main/java/com/medisoft/model/Address.java
@@ -0,0 +1,45 @@
+package com.medisoft.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "CITY")
+    private String city;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "COUNTRY")
+    private String country;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "COUNTY")
+    private String county;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "LINE")
+    private String line;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "POSTAL_CODE")
+    private String postalCode;
+
+}
---
+++ src/main/java/com/medisoft/model/Patient.java
@@ -0,0 +1,88 @@
+package com.medisoft.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "F_NAME")
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "M_NAME")
+    private String middleName;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "L_NAME")
+    private String lastName;
+
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull
+    private Date dateOfBirth;
+
+    @NotEmpty
+    @Size(min = 3,max = 40)
+    @Column(name = "EMAIL")
+    @Email(regexp = ".+[@].+[\\.].+")
+    private String email;
+
+    @NotEmpty
+    @Column(name = "GENDER")
+    private Character gender;
+
+    @NotEmpty
+    @Size(min = 3,max = 40)
+    @Column(name = "NATIONALITY")
+    private String nationality;
+
+    //https://www.baeldung.com/java-libphonenumber
+    //https://github.com/google/libphonenumber
+    //todo check the library for important phone validations and to discuss with Velikov abouth the telephone
+
+    @NotEmpty
+    @Size(min = 3,max = 40)
+    @Column(name = "TELEPHONE")
+    private String telephone;
+
+    @NotEmpty
+    @Column(name = "ACTIVE")
+    private Boolean active;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "SOCIAL_GROUP")
+    private String socialGroup;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "MARITAL_STATUS")
+    private String maritalStatus;
+
+    @NotEmpty
+    @Size(min = 3,max = 30)
+    @Column(name = "GENERAL_PRACTITIONER")
+    private String generalPractitioner;
+
+    //todo
+//    ADDRESS_ID NUMBER,
+//    CONTACT_PERSON_ID NUMBER
+
+}
